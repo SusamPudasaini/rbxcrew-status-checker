f9ce82c60ba610ed7185feeaa53375c3f71a326a
Susam Pudasaini <pudasainisusam@gmail.com>
2025-11-21 23:13:52 +0545

    nerd

--- api/checker.js
+++ api/checker.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  const apiUrl = "https://rbxcrew.com/api/user/claim?id=3&_rsc=478ms";
 
   const expected = { success: false, text: "Not Authorized." };
 
